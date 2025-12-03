efbd0ca2142c002a60e0bf0d8052c319d93de473
alsahas <168373030+alsahas@users.noreply.github.com>
2025-12-03 22:36:41 +0200

    Update database.js

--- database.js
+++ database.js
@@ -96,6 +96,7 @@ function distribute(cat)
 	var price="";
 	var quantity=0;
 	var display="none";
+	var dynamic_class="";
 	var page=document.getElementById("page");
 	if(page!=null)
 	{
@@ -119,7 +120,9 @@ function distribute(cat)
 						quantity=getItemQuantity(item.name);
 						if(quantity>0)display="block";
 						else display="none";
-						inner="<article class=\"product-card\"><a href=\"javascript:addProductToCart('"+key+"','"+item.name+"',"+item.price+","+item.pngExist+");\"><img class=\"prod-image\"src=\"png/products/"+im+".png\"></a><a href=\"javascript:addProductToCart('"+key+"','"+item.name+"',"+item.price+","+item.pngExist+");\"><img id=\"cart"+item.name+"\" class=\"cart\" style=\"display:"+display+"\"src=\"png/cart2.png\"></a><a href=\"javascript:plus('"+key+"','"+item.name+"',"+item.price+","+item.pngExist+");\"><img class=\"plus\" src=\"png/plus.png\"></a><a href=\"javascript:minus('"+item.name+"');\"><img class=\"minus\" src=\"png/minus.png\"></a><a href=\"javascript:remove('"+item.name+"');\"><img class=\"remove\" src=\"png/delete.png\"></a><p id=\"quantity"+item.name+"\"class=\"quantity\">"+quantity+"</p><p id=\"product"+item.name+"\"class=\"productname\"><span class=\"marquee-text-content\""+item.name+"</span></p><p id=\"price"+item.name+"\"class=\"productprice\">"+price+"</p></article>";
+						if(item.name.length>20)dynamic_class="is-scrolling";
+						else dynamic_class="text-content";
+						inner="<article class=\"product-card\"><a href=\"javascript:addProductToCart('"+key+"','"+item.name+"',"+item.price+","+item.pngExist+");\"><img class=\"prod-image\"src=\"png/products/"+im+".png\"></a><a href=\"javascript:addProductToCart('"+key+"','"+item.name+"',"+item.price+","+item.pngExist+");\"><img id=\"cart"+item.name+"\" class=\"cart\" style=\"display:"+display+"\"src=\"png/cart2.png\"></a><a href=\"javascript:plus('"+key+"','"+item.name+"',"+item.price+","+item.pngExist+");\"><img class=\"plus\" src=\"png/plus.png\"></a><a href=\"javascript:minus('"+item.name+"');\"><img class=\"minus\" src=\"png/minus.png\"></a><a href=\"javascript:remove('"+item.name+"');\"><img class=\"remove\" src=\"png/delete.png\"></a><p id=\"quantity"+item.name+"\"class=\"quantity\">"+quantity+"</p><p id=\"product"+item.name+"\"class=\"productname\"><span class=\""+dynamic_class+"\">"+item.name+"</span></p><p id=\"price"+item.name+"\"class=\"productprice\">"+price+"</p></article>";
 						inners+=inner;
 					}
 					
